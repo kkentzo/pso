4acc34d8f3004c74b752c9313b29371f3b42c8d6
Kyriakos Kentzoglanakis <kyriakos.kentzoglanakis@gmail.com>
2017-06-21 20:46:50 +0300

    Formatting and indentation

--- pso.h
+++ pso.h
@@ -14,7 +14,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see
    <http://www.gnu.org/licenses/>.
- */
+*/
 
 
 #ifndef PSO_H_
@@ -49,8 +49,8 @@
 // PSO SOLUTION -- Initialized by the user
 typedef struct {
 
-    double error;
-    double *gbest; // should contain DIM elements!!
+  double error;
+  double *gbest; // should contain DIM elements!!
 
 } pso_result_t;
 
@@ -64,28 +64,28 @@ typedef double (*pso_obj_fun_t)(double *, int, void *);
 // PSO SETTINGS
 typedef struct {
 
-    int dim; // problem dimensionality
-    double x_lo; // lower range limit
-    double x_hi; // higher range limit
-    double goal; // optimization goal (error threshold)
+  int dim; // problem dimensionality
+  double x_lo; // lower range limit
+  double x_hi; // higher range limit
+  double goal; // optimization goal (error threshold)
 
-    int size; // swarm size (number of particles)
-    int print_every; // ... N steps (set to 0 for no output)
-    int steps; // maximum number of iterations
-    int step; // current PSO step
-    double c1; // cognitive coefficient
-    double c2; // social coefficient
-    double w_max; // max inertia weight value
-    double w_min; // min inertia weight value
+  int size; // swarm size (number of particles)
+  int print_every; // ... N steps (set to 0 for no output)
+  int steps; // maximum number of iterations
+  int step; // current PSO step
+  double c1; // cognitive coefficient
+  double c2; // social coefficient
+  double w_max; // max inertia weight value
+  double w_min; // min inertia weight value
 
-    int clamp_pos; // whether to keep particle position within defined bounds (TRUE)
-                   // or apply periodic boundary conditions (FALSE)
-    int nhood_strategy; // neighborhood strategy (see PSO_NHOOD_*)
-    int nhood_size; // neighborhood size 
-    int w_strategy; // inertia weight strategy (see PSO_W_*)
+  int clamp_pos; // whether to keep particle position within defined bounds (TRUE)
+  // or apply periodic boundary conditions (FALSE)
+  int nhood_strategy; // neighborhood strategy (see PSO_NHOOD_*)
+  int nhood_size; // neighborhood size
+  int w_strategy; // inertia weight strategy (see PSO_W_*)
 
-    void *rng; // pointer to random number generator (use NULL to create a new RNG)
-    long seed; // seed for the generator
+  void *rng; // pointer to random number generator (use NULL to create a new RNG)
+  long seed; // seed for the generator
 
 } pso_settings_t;
 
@@ -104,5 +104,5 @@ void pso_solve(pso_obj_fun_t obj_fun, void *obj_fun_params,
 	       pso_result_t *solution, pso_settings_t *settings);
 
 
-    
+
 #endif // PSO_H_
